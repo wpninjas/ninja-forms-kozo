adb361cc0fad4b344707898b8af4f98f46361b88
Kyle B. Johnson <kyle@wpninjas.com>
2015-07-14 15:03:58 -0400

    fix templating values

--- kozo/boilerplate/includes/actions/{{name}}.php
+++ kozo/boilerplate/includes/actions/{{name}}.php
@@ -14,7 +14,7 @@ final class NF_{{NAME}}_Action extends NF_Notification_Base_Type
      */
     public function __construct()
     {
-        $this->name = __( '{{NAME}}', NF_{{NAME}}::TEXTDOMAIN );
+        $this->name = __( '{{Name}}', NF_{{NAME}}::TEXTDOMAIN );
 
         add_filter( 'nf_notification_types', array( $this, 'register_action_type' ) );
     }
