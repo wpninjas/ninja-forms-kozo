a166e4d35077258e50e548cb36c8355c8de143d0
Kyle B. Johnson <me@kylebjohnson.me>
2015-08-09 01:00:49 -0400

    update admin notice message

--- includes/templates/action-generate-admin-notice.html.php
+++ includes/templates/action-generate-admin-notice.html.php
@@ -1,3 +1,3 @@
 <div class="error">
-    <p><?php _e( 'Ninja Forms Kozo noticed an active redirect action for this form.' ); ?></p>
+    <p><?php _e( 'Ninja Forms Kozo noticed an active redirect action for this form. Redirect actions may break the download process for generated plugins.' ); ?></p>
 </div>
