cc02b75728b0010de00c0a35378f423516917075
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2016-02-08 13:24:21 -0500

    Update version compare in check.
    `3.0.0` is seen as newer than `3.0`, so the check could throw a false positive.

--- kozo/three-extension/ninja-forms-{{name}}.php
+++ kozo/three-extension/ninja-forms-{{name}}.php
@@ -12,7 +12,7 @@
  * Copyright {{YEAR}} {{AUTHOR}}.
  */
 
-if( version_compare( get_option( 'ninja_forms_version', '0.0.0' ), '3.0.0', '>' ) || get_option( 'ninja_forms_load_deprecated', FALSE ) ) {
+if( version_compare( get_option( 'ninja_forms_version', '0.0.0' ), '3.0', '>' ) || get_option( 'ninja_forms_load_deprecated', FALSE ) ) {
 
     include 'deprecated/ninja-forms-{{name}}.php';
 
